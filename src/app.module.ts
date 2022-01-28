4fd4766b25d096c98a9b66fb8bdcd7d845d34252
kevinMonteza <kmonteza95@gmail.com>
2022-01-28 01:49:36 -0500

    variable de base de datos oculta

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,10 @@ import { UserInterfaceModule } from './user-interface/user-interface.module';
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
-		MongooseModule.forRoot(
-			'mongodb+srv://admin:dvMNPVQU6L9IGQqG@cluster0.mwnzn.mongodb.net/mym_middleware_db?authSource=admin&replicaSet=atlas-som0en-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			},
-		),
+		MongooseModule.forRoot(process.env.MONGO_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}),
 		UserInterfaceModule,
 	],
 })
