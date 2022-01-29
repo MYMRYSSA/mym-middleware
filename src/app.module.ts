73c46da03cbcc61af79edca0dca66eaacf7c62da
kevinMonteza <kmonteza95@gmail.com>
2022-01-29 13:18:07 -0500

    fixed lintter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserInterfaceModule } from './user-interface/user-interface.module';
 
-console.log("🚀 ~ file: app.module.ts ~ line 10 ~ process.env.MONGO_URL", process.env.MONGO_URL)
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 		MongooseModule.forRoot(process.env.MONGO_URL, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-      
 		}),
 		UserInterfaceModule,
 	],
